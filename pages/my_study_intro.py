e1ebb0b9dea0fd5d05fe9330e94d9385edd96a51
米田一翔 <173421414+kazutoyoneda0130@users.noreply.github.com>
2025-08-15 08:08:07 +0900

    PDFの埋め込みを実装

--- pages/my_study_intro.py
+++ pages/my_study_intro.py
@@ -1,22 +1,27 @@
-import streamlit as st
-import my_func as my_func
+import base64
 import matplotlib.pyplot as plt
 import networkx as nx
-import sys
-import io
+import streamlit as st
+
+import my_func as my_func
 
 # 使用できる定式化をセット
 form_dict = {
-    "form1": "$x_{uv},z_{uk}$",
-    "form2": "$x_{uv},z_{iuk}$",
-    "from3": "$x_{uv},z_{uk},a_{ri}$"}
+    "定式化1": "$x_{uv},z_{uk},a_{ri}$",
+    "定式化2": "$x_{uv},z_{uk}$",
+    "定式化3": "$x_{uv},z_{iuk}$"}
 
 
 st.title("研究内容")
+# 研究内容の概要をPDFファイルを埋め込んで表示
 st.header("概要")
-st.write("""
-研究内容の説明をここに記載します。
-""")
+pdf_path = "修士論文概要.pdf"
+
+with open(pdf_path, "rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+encoded_pdf = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="600" type="application/pdf">'
+st.markdown(encoded_pdf, unsafe_allow_html=True)
+
 st.header("問題例の作成")
 if st.toggle('問題例作成'):
     # col1,col2,col3,col4 = st.columns([1,1,1,1])
@@ -24,7 +29,7 @@ if st.toggle('問題例作成'):
     with col1:
         car_num = st.number_input('車の数',step=1,min_value=1)
     with col2:
-        car_cap = st.number_input('車のキャパシティー',step=1,min_value=1)
+        car_cap = st.number_input('車の容量',step=1,min_value=1)
     # with col3:
     #     req_num = st.number_input('リクエストの数',step=1,min_value=1)
     with col4:
@@ -33,12 +38,12 @@ if st.toggle('問題例作成'):
     req_num = car_num * car_cap
 
     st.write(f"""
-             今回のコードではリクエスト数は車の数と車のキャパシティの積とします。
-             つまり、今回の入力では車の数が{car_num}、キャパシティが{car_cap}であるため、リクエスト数は{req_num}となります。
+             今回のコードでは「リクエスト数=車の数×車の容量」とします。
+             つまり、今回の入力では車の数が{car_num}、容量が{car_cap}であるため、リクエスト数は{req_num}となります。
              """)
 
     if car_num * car_cap < req_num:
-        st.error("車の数とキャパシティーの積はリクエストの数以上でなければなりません。")
+        st.error("車の数と容量の積はリクエストの数以上でなければなりません。")
     else:
         instance = my_func.CreateRideShareProblemInstance()
         instance.set_data(car_num=car_num, car_cap=car_cap, req_num=req_num, seed=seed, form="example")
